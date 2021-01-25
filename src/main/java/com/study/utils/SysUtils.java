2800a3a370f0a23f962d1be1cf58ebddb61f2d24
qianlicao51 <qianlicaodemo@163.com>
2021-01-25 09:47:03 +0800

    resourcePath

--- src/main/java/com/study/utils/SysUtils.java
+++ src/main/java/com/study/utils/SysUtils.java
@@ -2,6 +2,9 @@ package com.study.utils;
 
 import org.joda.time.DateTime;
 
+import java.io.File;
+import java.net.URL;
+
 /**
  * 工具类
  */
@@ -30,7 +33,26 @@ public class SysUtils {
         return new DateTime().toString("yyyy-MM-dd");
     }
 
+    /**
+     * 获取给定 路径的资源路劲
+     *
+     * @param path
+     * @return
+     */
+    public static String getResourcePath(String path) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = classLoader.getResource("");
+        File file = new File(resource.getPath(), path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return file.getPath();
+    }
+
     public static void main(String[] args) {
         System.out.println(new DateTime().toString("yyyy-MM-dd HH:mm:ss"));
+
+        final String resourcePath = getResourcePath("routes/tcp.xml");
+        System.out.println(resourcePath);
     }
 }
