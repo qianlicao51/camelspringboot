0a4abc5e57046661cb1a53f43cf0323b54bd4dc0
qianlicao51 <qianlicaodemo@163.com>
2021-01-27 16:51:32 +0800

    camel-jetty

--- src/main/java/com/study/jetty/HttpHandle.java
+++ src/main/java/com/study/jetty/HttpHandle.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author study
@@ -20,6 +21,7 @@ public class HttpHandle implements MessageHandler {
 
     @Override
     public void handler(Exchange exchange) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10);
         Message in = exchange.getIn();
         // 获取post ：raw|postman 提交POST数据 https://blog.csdn.net/qq_35275233/article/details/86548137
         String headbody = in.getBody(String.class);
@@ -30,9 +32,30 @@ public class HttpHandle implements MessageHandler {
         HttpServletRequest req = httpMessage.getRequest();
         HttpServletResponse response = httpMessage.getResponse();
 
-
+        response.setContentType("text/html; charset=GBK");
+        response.setCharacterEncoding("GBK");
         in.setBody(SysUtils.getDate());
         // exchange.getOut().setBody(SysUtils.getDateYmD());
+        // responsePrint(req,response,headbody);
 
+
+    }
+
+    /**
+     * 直接返回字符串内容，使用会有问题和jetty使用的servlet-api
+     *
+     * @param request  request
+     * @param response response
+     * @param str      发送字符串
+     */
+    protected void responsePrint(HttpServletRequest request, HttpServletResponse response, String str) {
+        try {
+            response.setContentType("text/html; charset=GBK");
+            response.setCharacterEncoding("GBK");
+            response.getWriter().print(str);
+            response.getWriter().close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
