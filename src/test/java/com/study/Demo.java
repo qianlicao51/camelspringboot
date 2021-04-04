17f4793a58951d0fcf27a46bbc770413d8c226a5
qianlicao51 <qianlicaodemo@163.com>
2021-04-04 22:14:47 +0800

    API

---
+++ src/test/java/com/study/Demo.java
@@ -0,0 +1,19 @@
+package com.study;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author MI
+ * @version 1.0
+ * @date 2021/4/4 18:38
+ */
+public class Demo {
+    public static void main(String[] args) {
+        final List<String> list = Arrays.asList("a", "v");
+        final Set<List<String>> lists = Collections.singleton(list);
+
+    }
+}
