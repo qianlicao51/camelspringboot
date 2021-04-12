71f93895f9bd49f140db36c32022afb571ddc3af
qianlicao51 <qianlicaodemo@163.com>
2021-04-12 15:51:16 +0800

    汪文君-高并发编程详解-9.3.3 类的初始化阶段

--- src/test/java/com/study/Demo.java
+++ src/test/java/com/study/Demo.java
@@ -11,7 +11,18 @@ import java.util.Set;
  * @date 2021/4/4 18:38
  */
 public class Demo {
+    private static int x = 10;
+
+    static {
+        //汪文君-高并发编程详解-9.3.3 类的初始化阶段
+        // 静态语句块只能对后面的变量进行赋值，但是不能对其进行访问
+        // System.out.println(x);
+        x = 100;
+    }
+
     public static void main(String[] args) {
+        //10
+        System.out.println(x);
         final List<String> list = Arrays.asList("a", "v");
         final Set<List<String>> lists = Collections.singleton(list);
 
