b2f1c44532e973ef6319584bc887cd1b9922df16
qianlicao51 <qianlicaodemo@163.com>
2021-06-24 16:35:26 +0800

    深入理解JVM|8|运行时栈帧结构

---
+++ src/test/java/com/study/jvm/char08/SoltDemo.java
@@ -0,0 +1,15 @@
+package com.study.jvm.char08;
+
+/**
+ * @author study
+ * @version 1.0
+ * @date 2021/6/24 16:11
+ */
+public class SoltDemo {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        byte[] placeholder = new byte[64 * 1024 * 1024];
+        System.gc();
+    }
+}
