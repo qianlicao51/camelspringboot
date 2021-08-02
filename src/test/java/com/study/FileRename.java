00accb1bcf55266d7b4b3ba3b22f0f91bf7a7f86
qianlicao51 <qianlicaodemo@163.com>
2021-08-02 23:12:42 +0800

    spring-demo|juc

--- src/test/java/com/study/FileRename.java
+++ src/test/java/com/study/FileRename.java
@@ -2,6 +2,8 @@ package com.study;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Collection;
@@ -25,6 +27,17 @@ public class FileRename {
         System.out.println(StringUtils.replaceEach("【www.gaoqing.tv】国产剧 青瓷 HunanTV.Green.Porcelain.Ep05.HDTV.720p.x264-CHDTV.mkv"
                 , new String[]{"【www.gaoqing.tv】", " 青瓷 HunanTV.Green.Porcelain.Ep", ".HDTV.720p.x264-CHDTV"}
                 , new String[]{"", "青瓷-", ""}));
+
+    }
+
+    @Test
+    public void listFile() {
+        String filePath = "F:\\A_WYM";
+        final Collection<File> files = FileUtils.listFiles(new File(filePath), new String[]{"mp4"}, false);
+        files.stream().sorted((a, b) -> new CompareToBuilder().append(b.length(), a.length()).toComparison()).forEach(file -> {
+            String name = file.getName();
+            System.out.println(String.format("ffmpeg -hwaccel cuvid -c:v h264_cuvid -i %s -c:v h264_nvenc -b 2M -y trans/%s", name, name));
+        });
 
     }
 }
---
+++ src/test/java/com/study/sgh/juc/demo/Demo01.java
@@ -0,0 +1,7 @@
+package com.study.sgh.juc.demo;
+
+public class Demo01 {
+    //尚硅谷JUC
+    //https://www.bilibili.com/video/BV1Kw411Z7dF?from=search&seid=11802483878648691416
+
+}
